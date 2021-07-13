0c3d448f65efb2c1611db7ac31a70244aec114ce
fabricioaraujo <araujopa@gmail.com>
2021-07-13 16:00:15 -0300

    13/07/2021: Criação do menu

--- agenda_aula.py
+++ agenda_aula.py
@@ -1,3 +1,34 @@
 #agenda: lista
 #contato: dicionario
 agenda = [] #lista
+
+def menu():
+    opcao = None #null, void
+    while opcao != "6":
+        print("Escolha uma das opções abaixo:")
+        print("1 - Incluir contato")
+        print("2 - Buscar contato")
+        print("3 - Editar contato")
+        print("4 - Excluir contato")
+        print("5 - Listar contatos")
+        print("6 - Sair")
+
+        opcao = input("Opção desejada: ")
+        
+        if opcao == "1":
+            incluir(agenda)
+        elif opcao == "2":
+            buscar(agenda)
+        elif opcao == "3":
+            editar(agenda)
+        elif opcao == "4":
+            apagar(agenda)
+        elif opcao == "5":
+            listar(agenda)
+        elif opcao == "6":
+            print("Bye bye :)")
+        else:
+            print("Opção inválida, tente novamente!")
+
+#Execução
+menu()
